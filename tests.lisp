635e16609c7987913d433e36e6eab2b4c290769e
tailor <dlowe@bitmuse.com>
2007-11-18 15:01:35 +0000

    [project @ Fixed tests to operate with nanosecond patch]

--- tests.lisp
+++ tests.lisp
@@ -9,16 +9,16 @@
 (in-suite* #:local-time.make :in :local-time)
 
 (test make-local-time
-  (let ((local-time (make-local-time :usec 1 :sec 2 :day 3 :timezone *default-timezone*)))
+  (let ((local-time (make-local-time :nsec 1 :sec 2 :day 3 :timezone *default-timezone*)))
     (is-every =
-      (usec-of local-time) 1
+      (nsec-of local-time) 1
       (sec-of local-time) 2
       (day-of local-time) 3)))
 
 (defmacro defcmptest (compare-with &body args)
   `(test ,compare-with
-    (flet ((make (day &optional (sec 0) (usec 0))
-             (make-local-time :day day :sec sec :usec usec)))
+    (flet ((make (day &optional (sec 0) (nsec 0))
+             (make-local-time :day day :sec sec :nsec nsec)))
       ,@(loop for entry in args
               if (= (length entry) 1)
               do (push 'is entry)
@@ -116,10 +116,10 @@
 
 (test local-time=-1
   (is (local-time= (make-local-time) (make-local-time)))
-  (is-false (local-time= (make-local-time) (make-local-time :usec 1))))
+  (is-false (local-time= (make-local-time) (make-local-time :nsec 1))))
 
 (test local-time/=
-  (is (local-time/= (make-local-time) (make-local-time :usec 1)))
+  (is (local-time/= (make-local-time) (make-local-time :nsec 1)))
   (is-false (local-time/= (make-local-time) (make-local-time))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -127,27 +127,27 @@
 (test encode-local-time
   (let ((local-time (encode-local-time 0 0 0 0 1 3 2000)))
     (is-every =
-      (usec-of local-time) 0
+      (nsec-of local-time) 0
       (day-of local-time) 0
       (sec-of local-time) 0))
   (let ((local-time (encode-local-time 0 0 0 0 29 2 2000)))
     (is-every =
-      (usec-of local-time) 0
+      (nsec-of local-time) 0
       (day-of local-time) -1
       (sec-of local-time) 0))
   (let ((local-time (encode-local-time 0 0 0 0 2 3 2000)))
     (is-every =
-      (usec-of local-time) 0
+      (nsec-of local-time) 0
       (sec-of local-time) 0
       (day-of local-time) 1))
   (let ((local-time (encode-local-time 0 0 0 0 1 1 2000)))
     (is-every =
-      (usec-of local-time) 0
+      (nsec-of local-time) 0
       (sec-of local-time) 0
       (day-of local-time) -60))
   (let ((local-time (encode-local-time 0 0 0 0 1 3 2001)))
     (is-every =
-      (usec-of local-time) 0
+      (nsec-of local-time) 0
       (sec-of local-time) 0
       (day-of local-time) 365)))
 
@@ -176,7 +176,7 @@
   (let ((local-time (make-local-time
                      :day (- (random 65535) 36767)
                      :sec (random 86400)
-                     :usec (random 1000))))
+                     :nsec (random 1000))))
     (multiple-value-bind (ms ss mm hh day mon year) (decode-local-time local-time)
       (is (local-time= local-time (encode-local-time ms ss mm hh day mon year))))))
 
@@ -185,43 +185,43 @@
 (test format-timestring
   (is-every string=
     "2008-06-05T04:03:02.000001"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-timezone-part-p t)
 
     "2008-06-05T04:03:02.000001-05:00"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008
                                           :timezone (local-time::make-timezone
                                                      :subzones `((,(* (* 60 5) -60) nil "anonymous" nil nil))
                                                      :loaded t)))
 
     "2008-06-05T04:03:02.000001Z"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008 :timezone +utc-zone+) :use-zulu-p t)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008 :timezone +utc-zone+) :use-zulu-p t)
 
-    "2008-06-05T04:03:02.12345678+00:00"
-    (format-timestring (encode-local-time 12345678 2 3 4 5 6 2008 :timezone +utc-zone+) :use-zulu-p nil)
+    "2008-06-05T04:03:02.1234567+00:00"
+    (format-timestring (encode-local-time 1234567890 2 3 4 5 6 2008 :timezone +utc-zone+) :use-zulu-p nil)
 
     "-06-05T04:03:02.000001"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t :date-elements 2)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-timezone-part-p t :date-elements 2)
 
     "-05T04:03:02.000001"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t :date-elements 1)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-timezone-part-p t :date-elements 1)
 
     "04:03:02.000001"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t)
 
     "04:03:02"
     (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t :time-elements 3)
 
     "-0005-06-05T04:03:02.000001"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 -5) :omit-timezone-part-p t)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 -5) :omit-timezone-part-p t)
 
     ""
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-time-part-p t :omit-date-part-p t)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-time-part-p t :omit-date-part-p t)
 
     "04"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t :time-elements 1)
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t :time-elements 1)
 
     "04:03"
-    (format-timestring (encode-local-time 1 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t :time-elements 2)))
+    (format-timestring (encode-local-time 1000 2 3 4 5 6 2008) :omit-timezone-part-p t :omit-date-part-p t :time-elements 2)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -242,7 +242,7 @@
   
 (test local-time
   (let ((now (now)))
-    (setf (usec-of now) 0)
+    (setf (nsec-of now) 0)
     (is (local-time= now
                      (local-time :unix (unix-time now)))))
   (let ((now (get-universal-time)))
@@ -286,11 +286,11 @@
                                      :start 5
                                      :end 15))))
   (is (eql (day-of (parse-timestring "2006-06-06TZ")) 2288))
-  (is (local-time= (encode-local-time 20000 3 4 5 6 7 2008)
+  (is (local-time= (encode-local-time 20000000 3 4 5 6 7 2008)
                    (parse-timestring "2008-07-06T05:04:03,02")))
   (is (local-time= (encode-local-time 0 02 0 0 23 3 2000)
                    (parse-timestring "--23T::02" :allow-missing-elements-p t)))
-  (is (local-time= (encode-local-time 80000 7 6 5 1 3 2000)
+  (is (local-time= (encode-local-time 80000000 7 6 5 1 3 2000)
                    (parse-timestring "T05:06:07,08"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -323,7 +323,7 @@
 
 (test read-universal-time
   (let ((now (now)))
-    (setf (usec-of now) 0)
+    (setf (nsec-of now) 0)
     (is (local-time= now
                      (with-input-from-string (ins (princ-to-string (universal-time now)))
                        (local-time::read-universal-time ins #\@ nil))))))
@@ -363,9 +363,9 @@
           (loop for month :from 1 :to 12 do
                 (loop for day :in '(1 2 3 27 28 29 30 31) do
                       (when (valid-date-p year month day)
-                        (multiple-value-bind (usec sec minute hour day* month* year* day-of-week daylight-p zone)
+                        (multiple-value-bind (nsec sec minute hour day* month* year* day-of-week daylight-p zone)
                             (decode-local-time (encode-local-time 0 0 0 0 day month year))
-                          (declare (ignore usec sec minute hour day-of-week daylight-p zone))
+                          (declare (ignore nsec sec minute hour day-of-week daylight-p zone))
                           (is (= year year*))
                           (is (= month month*))
                           (is (= day day*)))))))))
