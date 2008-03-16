e8e3a1c9cad538aa5c8b703278a95cec60828295
tailor <attila.lendvai@gmail.com>
2008-03-16 18:25:28 +0000

    [project @ Follow cl-postmodern's timestamp-with-timezone changes]

--- src/integration/cl-postgres.lisp
+++ src/integration/cl-postgres.lisp
@@ -7,39 +7,40 @@
 (defconstant +postgres-day-offset-to-local-time+ -60)
 
 (defun set-local-time-cl-postgres-readers (&optional (table cl-postgres:*sql-readtable*))
-  (cl-postgres:set-sql-datetime-readers
-   :table table
-   :date
-   (lambda (days)
-     (local-time:make-local-time
-      :nsec 0 :sec 0 :day (+ days +postgres-day-offset-to-local-time+)
-      :timezone local-time:+utc-zone+))
-   :timestamp
-   (lambda (usecs)
-     (multiple-value-bind (days usecs)
-         (floor usecs +usecs-per-day+)
-       (multiple-value-bind (secs usecs)
-           (floor usecs 1000000)
-         (local-time:make-local-time :nsec (* usecs 1000)
-                                     :sec secs
-                                     :day (+ days +postgres-day-offset-to-local-time+)
-                                     :timezone local-time:+utc-zone+))))
-   :interval
-   (lambda (months days usecs)
-     (declare (ignore months days usecs))
-     (error "Intervals are not yet supported"))
-   :time
-   (lambda (usecs)
-     (multiple-value-bind (days usecs)
-         (floor usecs +usecs-per-day+)
-       (assert (= days 0))
-       (multiple-value-bind (secs usecs)
-           (floor usecs 1000000)
-         (local-time:make-local-time
-          :nsec (* usecs 1000)
-          :sec secs
-          :day 0
-          :timezone local-time:+utc-zone+))))))
+  (flet ((timestamp-reader (usecs)
+           (multiple-value-bind (days usecs)
+               (floor usecs +usecs-per-day+)
+             (multiple-value-bind (secs usecs)
+                 (floor usecs 1000000)
+               (local-time:make-local-time :nsec (* usecs 1000)
+                                           :sec secs
+                                           :day (+ days +postgres-day-offset-to-local-time+)
+                                           :timezone local-time:+utc-zone+)))))
+    (cl-postgres:set-sql-datetime-readers
+     :table table
+     :date
+     (lambda (days)
+       (local-time:make-local-time
+        :nsec 0 :sec 0 :day (+ days +postgres-day-offset-to-local-time+)
+        :timezone local-time:+utc-zone+))
+     :timestamp #'timestamp-reader
+     :timestamp-with-timezone #'timestamp-reader
+     :interval
+     (lambda (months days usecs)
+       (declare (ignore months days usecs))
+       (error "Intervals are not yet supported"))
+     :time
+     (lambda (usecs)
+       (multiple-value-bind (days usecs)
+           (floor usecs +usecs-per-day+)
+         (assert (= days 0))
+         (multiple-value-bind (secs usecs)
+             (floor usecs 1000000)
+           (local-time:make-local-time
+            :nsec (* usecs 1000)
+            :sec secs
+            :day 0
+            :timezone local-time:+utc-zone+)))))))
 
 (defmethod cl-postgres:to-sql-string ((arg local-time:local-time))
   (local-time:format-rfc3339-timestring arg))
