834bb8201728e43e9d2009e20322784288c9618d
Daniel Lowe <dlowe@bitmuse.com>
2008-06-11 15:28:34 +0000

    cl-postgres sql string uses utc-zone explicitly

--- src/integration/cl-postgres.lisp
+++ src/integration/cl-postgres.lisp
@@ -40,4 +40,4 @@
             :day 0)))))))
 
 (defmethod cl-postgres:to-sql-string ((arg local-time:timestamp))
-  (local-time:format-rfc3339-timestring arg))
+  (local-time:format-rfc3339-timestring nil arg :timezone +utc-zone+)))
