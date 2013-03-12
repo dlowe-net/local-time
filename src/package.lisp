a8fb21c17f5c98d9c6e8955da1bdf9f23e147903
Andy Chambers <achambers.home@gmail.com>
2013-03-12 14:07:49 +0000

    Convert tabs to spaces

--- src/package.lisp
+++ src/package.lisp
@@ -54,9 +54,9 @@
            #:timestamp-subtimezone
            #:define-timezone
            #:*default-timezone*
-	   #:*clock*
-	   #:clock-now
-	   #:clock-today
+           #:*clock*
+           #:clock-now
+           #:clock-today
            #:find-timezone-by-location-name
            #:now
            #:today
