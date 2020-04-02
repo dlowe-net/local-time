9a147302ea3fae964a9ef6a27b7677c51eb6a308
egao1980 <egao1980@gmail.com>
2020-04-03 00:49:52 +0100

    Proper abbreviation search
    Making full use of transition history. Returning multiple values to provide insight into type of matches

--- src/local-time.lisp
+++ src/local-time.lisp
@@ -188,6 +188,32 @@
 ;; time of day.
 (defparameter +modified-julian-date-offset+ -51604)
 
+(defun %subzone-as-of (timezone &key unix-time timestamp)
+  (let* ((as-of-time (cond
+                       (unix-time unix-time)
+                       (timestamp (timestamp-to-unix timestamp))
+                       (t nil)))
+         (index-length (length (timezone-indexes timezone)))
+         (transition-idx (cond ((zerop index-length) nil)
+                               (as-of-time (transition-position as-of-time
+                                                                (timezone-transitions timezone)))
+                               (t (1- index-length))))
+         (subzone-idx (if transition-idx 
+                          (elt (timezone-indexes timezone) transition-idx)
+                          0)))
+    (values (elt (timezone-subzones timezone) subzone-idx)
+            transition-idx)))
+
+(defun %subzone-previous-transition-time (timezone subzone-idx transition-idx)
+  (when transition-idx
+    (let ((prev-idx (position subzone-idx
+                              (timezone-indexes timezone)
+                              :end transition-idx
+                              :from-end t)))
+      (when prev-idx
+        (elt (timezone-transitions timezone) prev-idx)))))
+
+
 (defun %guess-offset (seconds days &optional timezone)
   ;; try converting the local time to a timestamp using each available
   ;; subtimezone, until we find one where the offset matches the offset that
@@ -198,12 +224,7 @@
   ;; the one that we pick to resolve ambiguous local time representations.
   (let* ((zone (%realize-timezone (or timezone *default-timezone*)))
          (unix-time (timestamp-values-to-unix seconds days))
-         (subzone-idx (if (zerop (length (timezone-indexes zone)))
-                          0
-                          (elt (timezone-indexes zone)
-                               (transition-position unix-time
-                                                    (timezone-transitions zone)))))
-         (subzone (elt (timezone-subzones zone) subzone-idx)))
+         (subzone (%subzone-as-of zone :unix-time unix-time)))
     (subzone-offset subzone)))
 
 (defun %read-binary-integer (stream byte-count &optional (signed nil))
@@ -402,17 +423,32 @@
     (error "Seems like the timezone repository has not yet been loaded. Hint: see REREAD-TIMEZONE-REPOSITORY."))
   (gethash name *location-name->timezone*))
 
-(defun find-timezones-by-abbreviated-subzone-name (abbreviated-name)
-  "Returns a list of timezones that have ABBREVIATED-NAME subzone. Sorted in order of appearance of ABBREVIATED-NAME  in subsones list."
-  (stable-sort 
-   (copy-list (gethash abbreviated-name *abbreviated-subzone-name->timezone-list*))
-   #'<
-   :key (lambda (timezone)
-          (position
-           abbreviated-name
-           (timezone-subzones timezone)
-           :key #'subzone-abbrev
-           :test #'string=))))
+(defun find-timezone-by-abbreviated-subzone-name (abbreviated-name &key unix-time timestamp)
+  "Returns a timezone that have active ABBREVIATED-NAME subzone. If multiple active timezones match then list of matches will be returned as a second value. All other matches (historical or future) will be returned as a third value. Extra matches are returned paired with latest known transition time for the matching subzone."
+  (loop for zone in (gethash abbreviated-name *abbreviated-subzone-name->timezone-list*)
+        ;; get the subzone and the latest transition index
+        for (subzone transition-idx) = (multiple-value-list (%subzone-as-of zone :unix-time unix-time :timestamp timestamp))
+        if (equal abbreviated-name (subzone-abbrev subzone))
+        collect (list zone (elt (timezone-transitions zone) transition-idx)) into current
+        else
+        when transition-idx
+        collect (list zone
+                      ;; find maximum transition timestamp preceeding transition-idx's for all subzones with matching abbrev name
+                      (loop for subzone-idx from 0 to (1- (length (timezone-subzones zone)))
+                            for sz = (elt (timezone-subzones zone) subzone-idx)
+                            for tix = (%subzone-previous-transition-time zone subzone-idx transition-idx)
+                            when (and tix (equal abbreviated-name (subzone-abbrev sz)))
+                            maximize tix))
+        into other
+        finally (return
+                  (values
+                   (caar current)
+                   current
+                   (stable-sort
+                    (remove-if #'null other :key #'cadr)
+                    #'>
+                    :key #'cadr)
+                   ))))
 
 (defun timezone= (timezone-1 timezone-2)
   "Return two values indicating the relationship between timezone-1 and timezone-2. The first value is whether the two timezones are equal and the second value indicates whether it is sure or not.
